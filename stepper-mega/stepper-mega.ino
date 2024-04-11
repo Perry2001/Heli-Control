c73c027e75b27a71165290288dc1f4a6e0760faf
John Perry <93684750+Perry2001@users.noreply.github.com>
2024-04-11 17:25:58 +0800

    minor bug fixes

--- stepper-mega/stepper-mega.ino
+++ stepper-mega/stepper-mega.ino
@@ -1,48 +1,42 @@
-    /*
-      e-Gizmo Microstep Driver Sample code
+int STP = 4; 
+int DIR = 3; 
+int delay_time = 10;
+int speedValue = 0; // Variable to store the received speed value
 
+void setup() {
+  pinMode(STP, OUTPUT); // STP pin as output
+  pinMode(DIR, OUTPUT); // DIR pin as output
+  digitalWrite(DIR, HIGH); // Initial direction
+  Serial.begin(115200); // Initialize serial communication
+}
 
-      *""""Wiring connections:"""*
-       Gizduino MCU  to  Microstep
-         D3  -------------> DIR
-         D4  -------------> STP/CLK
-         GND -------------> GND
-         +5V -------------> VIN
+void loop() {
+  if (Serial.available() > 0) { // Check if there's data available to read
+    String data = Serial.readStringUntil('\n'); // Read the incoming data until newline character
+    if (data.startsWith("speed:")) { // Check if the received data starts with "speed:"
+      data.remove(0, 6); // Remove the "speed:" prefix
+      speedValue = data.toInt(); // Convert the remaining string to an integer
+      Serial.print("Speed value received: ");
+      Serial.println(speedValue); // Print the received speed value
 
-      by e-Gizmo Mechatronix Central
-      http://www.e-gizmo.com
-      February 18, 2016
+      // Map the speed value to delay_ms
+      int delay_ms = map(speedValue, 0, 30, 2000, 500);
+      Serial.print("Delay set to: ");
+      Serial.println(delay_ms); // Print the mapped delay value
 
-     */
-
-  //Pin assignments
-  int STP = 4; 
-  int DIR = 3; 
-  int delay_ms = 1000;
-  int delay_time = 10;
-    void setup() {
-      //make the pins as an output
-      pinMode(STP,OUTPUT); 
-      pinMode(DIR,OUTPUT);
-    }
-    void loop() {
-      // digitalWrite(DIR,HIGH); // The motor moves in a particular direction
-      // makes 100000 pulses
+      // Perform stepping with the calculated delay_ms
       for(int x = 0; x < 10000; x++) {
-        digitalWrite(STP,HIGH); 
+        digitalWrite(STP, HIGH); 
         delayMicroseconds(delay_ms); 
-        digitalWrite(STP,LOW); 
+        digitalWrite(STP, LOW); 
         delayMicroseconds(delay_ms); 
+        
+        if (Serial.available() > 0) { // Check if new speed data is available
+          break; // Exit the loop to apply the new speed immediately
+        }
       }
-      delay(delay_time); // One second delay
-      
-      digitalWrite(DIR,LOW); //Changes the direction
-      // makes 10000 pulses
-      for(int x = 0; x < 10000; x++) {
-        digitalWrite(STP,HIGH);
-        delayMicroseconds(delay_ms);
-        digitalWrite(STP,LOW);
-        delayMicroseconds(delay_ms);
-      }
-      delay(delay_time);
     }
+  }
+
+  delay(delay_time); // One second delay
+}
